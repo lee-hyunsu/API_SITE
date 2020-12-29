5c1a45beebaa7ebf097d2014f55e95e76412a6ea
lee-hyunsu <dlgustn675@naver.com>
2020-12-30 01:14:17 +0900

    test

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddlerware from "redux-promise";
+import reduxThunk from "redux-thunk";
+
+
+const createStoreWidthMiddleware = applyMiddleware(
+  promiseMiddlerware,
+  reduxThunk
+)(createStore);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWidthMiddleware(
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
